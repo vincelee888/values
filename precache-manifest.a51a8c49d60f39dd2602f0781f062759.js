3e148c533ff4f8a0a3a11e336e529c3a0fdc7acf
vincelee888 <vincent.lee@auden.co.uk>
2020-09-04 15:12:14 +0100

    Updates

---
+++ precache-manifest.a51a8c49d60f39dd2602f0781f062759.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f67ba6015e2ac5f64ed13245f5540425",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ed0a8648abfc9971b66d",
+    "url": "./static/css/main.1982dd6e.chunk.css"
+  },
+  {
+    "revision": "3502323baca5f4ad68fa",
+    "url": "./static/js/2.7bee308b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.7bee308b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed0a8648abfc9971b66d",
+    "url": "./static/js/main.24a0620a.chunk.js"
+  },
+  {
+    "revision": "cfedf57b116e7f67b527",
+    "url": "./static/js/runtime-main.3957affa.js"
+  }
+]);
